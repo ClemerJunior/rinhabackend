5b039cbc0979e67c1f4580b6e71131f0891e2b34
clemerJunior <clemer.junior@hotmail.com>
2024-02-17 20:37:00 -0300

    fix mapping

--- src/main/java/com/clemer/rinha/controller/ClienteController.java
+++ src/main/java/com/clemer/rinha/controller/ClienteController.java
@@ -5,18 +5,16 @@ import com.clemer.rinha.domain.dto.TransacaoResponseDTO;
 import com.clemer.rinha.service.ClienteService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@RequestMapping
 @RequiredArgsConstructor
+@RestController
+@RequestMapping("/clientes/")
 public class ClienteController {
 
     private final ClienteService clienteService;
 
-    @PostMapping("/clientes/{id}/transacoes")
+    @PostMapping("{id}/transacoes")
     public ResponseEntity<TransacaoResponseDTO> efetuarTransacao(
             @PathVariable Long id,
             @RequestBody TransacaoRequestDTO requestDTO) throws Exception {
@@ -24,3 +22,4 @@ public class ClienteController {
         return ResponseEntity.ok(clienteService.efetuarTransacao(id, requestDTO));
     }
 }
+

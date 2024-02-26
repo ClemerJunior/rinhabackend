d127cfbd9d77eee2cfc0c6aa13943c3ec4d2b0e2
clemerJunior <clemer.junior@hotmail.com>
2024-02-25 22:24:43 -0300

    configuraçẽos finais para docker compose

---
+++ .config/script.sql
@@ -0,0 +1,23 @@
+CREATE TABLE cliente (
+    id SMALLINT PRIMARY KEY,
+    nome VARCHAR(30),
+    limite INTEGER not null,
+    saldo INTEGER not null default 0
+);
+
+CREATE TABLE transacao (
+    id SMALLINT PRIMARY KEY,
+    client_id SMALLINT REFERENCES cliente(id) ON DELETE CASCADE,
+    valor INTEGER,
+    tipo CHAR(1),
+    descricao varchar(50),
+    realizada_em timestamp(6)
+);
+
+INSERT INTO cliente (id, nome, limite, saldo)
+VALUES
+    (1, 'o barato sai caro', 1000 * 100, 0),
+    (2, 'zan corp ltda', 800 * 100, 0),
+    (3, 'les cruders', 10000 * 100, 0),
+    (4, 'padaria joia de cocaia', 100000 * 100, 0),
+    (5, 'kid mais', 5000 * 100, 0);
